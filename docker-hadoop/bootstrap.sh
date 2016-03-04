650e446783d751ec47384770b9894d37fe55a7ab
craig <cam14@miga.cs.sfu.ca>
2016-03-03 16:46:30 -0800

    properly sets core-site.xml to a usable value by other clients in a docker cluster - i.e. now you can use the volume properly

--- docker-hadoop/bootstrap.sh
+++ docker-hadoop/bootstrap.sh
@@ -10,7 +10,9 @@ rm /tmp/*.pid
 cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
 # altering the core-site configuration
-sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+#$IP = $(/sbin/ip route | awk '/src/ { print $9 }')
+IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+sed s/HOSTNAME/$IP/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
