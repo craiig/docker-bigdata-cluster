9e636a4d663ded267ef003170387a7455235fe3a
craig <cam14@miga.cs.sfu.ca>
2016-05-10 18:13:44 -0700

    start of some utils for running in the spark console

---
+++ benchmark-accumulo/utils/util.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.SparkContext;
+
+object PerfUtils {
+  def getFreeMemory(sc: SparkContext){
+    //var mem:Long = 0;
+    sc.getExecutorMemoryStatus.foreach{ case (k,v) => {
+      println(k + " is using " + (v._1 - v._2) + "/" + v._1 + " bytes");
+      //mem += v._2;
+    } }
+    //println("Total: " + mem + " bytes");
+  }
+}
