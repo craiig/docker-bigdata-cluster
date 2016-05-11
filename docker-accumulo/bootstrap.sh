f57912082120ec522dda8d73d3893adbd68a470e
craig <cam14@miga.cs.sfu.ca>
2016-05-10 18:17:11 -0700

    working accumulo

--- docker-accumulo/bootstrap.sh
+++ docker-accumulo/bootstrap.sh
@@ -2,8 +2,16 @@
 
 : ${ACCUMULO_HOME:=/usr/local/accumulo}
 
+#keep an up to date ip in the accumulo configuration
+IP=$(ifconfig eth0 | grep -oP '\d+\.\d+\.\d+\.\d+' | head -n1)
+IPCONFIGS="$ACCUMULO_HOME/conf/masters $ACCUMULO_HOME/conf/slaves"
+for conf in $IPCONFIGS; do
+	if [[ -z "$(grep $IP $conf)" ]]; then
+		echo $IP >> $conf
+	fi;
+done;
 
-#printf 'default\naccumulo\naccumulo\n' | $ACCUMULO_HOME/bin/accumulo init
+printf 'accumulo\naccumulo\naccumulo\n' | $ACCUMULO_HOME/bin/accumulo init
 $ACCUMULO_HOME/bin/start-all.sh
 
 if [[ $1 == "-d" ]]; then
