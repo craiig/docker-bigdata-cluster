31485ac2b4ff023190aff148f03cd60ad61cf20b
craig@octavia <craiig@gmail.com>
2016-05-16 14:17:27 -0700

    updated some util scripts for configuration

--- util/sudo_docker_setup.sh
+++ util/sudo_docker_setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
-
 apt-get update
+apt-get install docker-engine
+service docker start
+docker run hello-world
--- util/useradd.sh
+++ util/useradd.sh
@@ -1,4 +1,8 @@
 GROUP=38264 #systems group id
+if [[ $1 == "" ]]; then
+	echo "usage: $0 <username>"
+	exit 1
+fi
 echo sudo adduser --gid $GROUP $1
 echo sudo adduser $1 sudo
 echo sudo adduser $1 docker
