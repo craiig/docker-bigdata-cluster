7cf8464f4139205c10ae8fe6cfd68d891f68e5f6
craig <cam14@miga.cs.sfu.ca>
2016-05-24 11:43:50 -0700

    added tiny webserver

---
+++ util/webserver/server.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('./'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
