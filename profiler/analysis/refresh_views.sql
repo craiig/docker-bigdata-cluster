cc19aff51b4626d3635130bc4f7e8adcf8f88cdc
craig <cam14@miga.cs.sfu.ca>
2016-06-06 12:02:18 -0700

    added data dependency to scala kmeans

--- profiler/analysis/refresh_views.sql
+++ profiler/analysis/refresh_views.sql
@@ -1,15 +1,15 @@
+-- begin;
+-- refresh materialized view interpreter_breakdown;
+-- end;
+-- begin;
+-- refresh materialized view perf_profiles_facts;
+-- end;
+-- begin;
+-- refresh materialized view perf_profiles_times;
+-- end;
 begin;
-refresh materialized view interpreter_breakdown;
+refresh materialized view unique_top_stacks; --failed due to lack of space, needs refresh
 end;
-begin;
-refresh materialized view perf_profiles_facts;
-end;
-begin;
-refresh materialized view perf_profiles_times;
-end;
-begin;
-refresh materialized view unique_top_stacks;
-end;
-begin;
-refresh materialized view highlevel_breakdowns;
-end;
+-- begin;
+-- refresh materialized view highlevel_breakdowns;
+-- end;
