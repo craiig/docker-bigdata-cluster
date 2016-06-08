106491e52f8c5b762d4ad96c3f55fb0c6e9bf337
Steven Hall <Hallzy.18@gmail.com>
2016-06-08 16:19:27 -0700

    Modifed scala code to execute the kmeans benchmark
    with our larger dataset

--- benchmarks/kmeans/kmeans-scala/kmeans.scala
+++ benchmarks/kmeans/kmeans-scala/kmeans.scala
@@ -2,16 +2,26 @@ import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.Row
+
 
 object benchKmeans {
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setAppName("K-means Benchmark")
-    val sc   = new SparkContext(conf)
+    val conf       = new SparkConf().setAppName("K-means Benchmark")
+    val sc         = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    // Load and parse the data
-    val data       = sc.textFile("reviews_books_first_1000.json")
-    val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+    val json_file = "reviews_books_first_1000.json"
+
+    // Create the DataFrame
+    val df = sqlContext.read.json(json_file)
+
+    val parsedData = df.map(t => Vectors.dense(
+                              t.getAs[Double]("overall"),
+                              t.getAs[String]("reviewText").length().toDouble,
+                              t.getAs[Long]("unixReviewTime").toDouble
+                                              )).cache()
 
     // Cluster the data into two classes using KMeans
     val numClusters   = 2
@@ -20,10 +30,9 @@ object benchKmeans {
 
     // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedData)
-    println("Within Set Sum of Squared Errors = " + WSSSE)
+      println("Within Set Sum of Squared Errors = " + WSSSE)
 
     // Save and load model
-
     clusters.save(sc, "myModelPath")
     val sameModel = KMeansModel.load(sc, "myModelPath")
   }
