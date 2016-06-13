5a33c9d100c19d8e8bf055946c12925359de52dc
Aziz Alghamdi <abdulaziz.inbox@gmail.com>
2016-06-13 14:35:44 -0700

    Modified kmeans-python to use amazon reviews dataset Got rid of virtualenv in Makefile

--- benchmarks/kmeans/kmeans-python/kmeans.py
+++ benchmarks/kmeans/kmeans-python/kmeans.py
@@ -1,17 +1,20 @@
 from pyspark.mllib.clustering import KMeans, KMeansModel
-from numpy import array
+# from numpy import array
 from math import sqrt
 from pyspark import SparkContext
+import json
 
 # Load and parse the data
-sc = SparkContext("local", "K-Means")
+sc = SparkContext("local", "Python K-Means Amazon Reviews")
 
-data = sc.textFile('kmeans_data.txt')
-parsedData = data.map(lambda line: array([float(x) for x in line.split(' ')]))
+data = sc.textFile('reviews_books_first_1000.json')
+parsedData = data.map(lambda line: json.loads(line)).\
+             map(lambda line: (line['overall'], len(line['reviewText'])))
 
 # Build the model (cluster the data)
 clusters = KMeans.train(parsedData, 2, maxIterations=10,
-        runs=10, initializationMode="random")
+                        runs=10, initializationMode="random")
+
 
 # Evaluate clustering by computing Within Set Sum of Squared Errors
 def error(point):
