0013c98d72e720ea7d03e668b66c9324e578e92e
Steven Hall <Hallzy.18@gmail.com>
2016-07-11 12:27:13 -0700

    Added config to enable all tests for spark-perf

--- benchmarks/spark-perf/run-script.sh
+++ benchmarks/spark-perf/run-script.sh
@@ -19,7 +19,6 @@ function default_config {
     sed -i '/SPARK_DRIVER_MEMORY = /c\SPARK_DRIVER_MEMORY = \"1g\"' $CONFIG_PATH/config.py
     sed -i '/spark\.executor\.memory/c\# JavaOptionSet(\"spark\.executor\.memory\", \[\"2g\"\]),' $CONFIG_PATH/config.py
   elif [[ "$HOSTNAME" == "octavia" ]]; then
-    echo "hi"
     sed -i '/SPARK_CLUSTER_URL = /c\SPARK_CLUSTER_URL = \"spark:\/\/octavia:7077\"' "$CONFIG_PATH"/config.py
     sed -i '/SCALE_FACTOR = /c\SCALE_FACTOR = 0.05' "$CONFIG_PATH"/config.py
     sed -i '/SPARK_DRIVER_MEMORY = /c\SPARK_DRIVER_MEMORY = \"1g\"' "$CONFIG_PATH"/config.py
@@ -47,6 +46,17 @@ if [[ "$1" == "config" ]]; then
 
   # set spark_home_dir to $SPARK_BIN
   sed -i '/SPARK_HOME_DIR = /c\SPARK_HOME_DIR = \"'"$SPARK_BIN"'\"' $CONFIG_PATH/config.py
+
+  sed -i '/RUN_SPARK_TESTS = /c\RUN_SPARK_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/RUN_PYSPARK_TESTS = /c\RUN_PYSPARK_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/RUN_STREAMING_TESTS = /c\RUN_STREAMING_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/RUN_MLLIB_TESTS = /c\RUN_MLLIB_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/RUN_PYTHON_MLLIB_TESTS = /c\RUN_PYTHON_MLLIB_TESTS = True' $CONFIG_PATH/config.py
+
+  sed -i '/PREP_SPARK_TESTS = /c\PREP_SPARK_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/PREP_PYSPARK_TESTS = /c\PREP_PYSPARK_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/PREP_STREAMING_TESTS = /c\PREP_STREAMING_TESTS = True' $CONFIG_PATH/config.py
+  sed -i '/PREP_MLLIB_TESTS = /c\PREP_MLLIB_TESTS = True' $CONFIG_PATH/config.py
 
   if [[ "$2" == "local" ]]; then
     echo "Create a config for local execution"
