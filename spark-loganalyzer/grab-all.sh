8f2cd9836ca98d0f78e1ed26525f2c2f84786e4e
craiig <craiig@gmail.com>
2016-09-14 15:26:37 -0700

    using honest profiler for spark stack tracing instead

--- spark-loganalyzer/grab-all.sh
+++ spark-loganalyzer/grab-all.sh
@@ -2,8 +2,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z "$1" ]; then
-	echo "usage $0 <application_id>"
+	echo "usage $0 <shell.log>"
 	exit 1
 fi
-$DIR/grab-logs-docker.sh $1
-$DIR/grab-event-logs-hdfs.sh $1
+
+application_id=$( grep -Po "application_\d+_\d+" $1 | head -n 1 )
+
+docker_id=$(docker ps | grep hadoop_master-$(whoami) | awk '{print $1}')
+
+$DIR/grab-logs-docker.sh $application_id $docker_id
+$DIR/grab-event-logs-hdfs.sh $application_id
+
+#after logs come back from docker we can check for an yourkit samples
+samples=$(grep -iPoh '(?<=CPU Sampling Stopped. Log stored at: )(.*)' container_*/stderr)
+
+if [ ! -z "$samples" ]; then
+	rm -rf samples
+	mkdir samples
+	for s in $samples; do
+		echo $s
+		docker cp $docker_id:$s samples/$(basename $s)
+		docker exec -t $docker_id rm $s
+		#docker exec -t $docker_id bash -c "sudo tar -c $s" | tar -x -C ./samples
+	done
+	#docker exec -t $docker_id bash -c "sudo tar -c $samples" | tar -x -C ./samples
+fi
