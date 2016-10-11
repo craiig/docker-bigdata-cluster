023ab7b501af4d9b5c01e4baf3911bf039c53f05
craiig <craiig@gmail.com>
2016-10-11 12:34:20 -0700

    improved stack logs filter

--- spark-loganalyzer/stack-logs-filter.py
+++ spark-loganalyzer/stack-logs-filter.py
@@ -3,12 +3,19 @@
 # filter sample files based on task ID
 # could extend this with arguments later
 
+#typically used in a shell script, like:
+# find samples/ -iname "*.hpl" | ../../stack-logs-filter.py 2313 | xargs -I{} cp {} samples_last/
+
 
 import sys, re
 
 def usage():
-    print "%s <task_number>"
+    print "%s <task_number>" % (sys.argv[0])
     print "returns all files that are greater or equal to given task number"
+
+if len(sys.argv) < 2:
+    usage()
+    sys.exit()
 
 file_pattern = re.compile("profile_.*task=(\d+)", re.IGNORECASE)
 split_number = int(sys.argv[1])
